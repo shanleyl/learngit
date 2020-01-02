9ca661ffed78b53eb26d701b6d60e331157b130f
shanleyl <1012905167@qq.com>
2020-01-02 16:09:37 +0800

    第一次

---
+++ service-feign/src/main/java/com/jk/Hystric/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.jk.Hystric;
+
+import com.jk.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry:"+name;
+    }
+}
